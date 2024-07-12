f6a51a3d28da0ab235c3ca9f30a9b2a0b07ea3ec
Mikail Korkmaz <mikail-korkmaz@web.de>
2024-07-12 13:35:00 +0200

    getting started with theming / setting & using a color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/widget/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-    home: const Expenses(),
-  ),
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+
+        ),
+      ),
+      home: const Expenses(),
+    ),
   );
 }
